3cd9c167c6f471b59fe4c3a26a2691a1d75a82ad
fsd66 <fsd66@rocketmail.com>
2021-03-30 00:06:50 -0400

    added more pekora song video embeds

---
+++ src/components/EmbeddedVideoRandomizer.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+export default class EmbeddedVideoRandomizer extends React.Component {
+    static defaultProps = {
+        videoSize: { width: 560, height: 315 },
+        allow: [
+            "accelerometer",
+            "autoplay",
+            "clipboard-write",
+            "clipboard-write",
+            "encrypted-media",
+            "gyroscope",
+            "picture-in-picture"
+        ],
+        videos: [
+            {
+                src: "https://www.youtube.com/embed/uRB1G0cKpIk",
+                title: "peko-miko-song"
+            },
+            {
+                src: "https://www.youtube.com/embed/_KTwDH_KQ_g",
+                title: "peko-random-brain-song"
+            },
+            {
+                src: "https://www.youtube.com/embed/AAr2lHZLNyU",
+                title: "discommunication-alien-pekover-song"
+            }
+        ]
+    }
+    render() {
+        const selectedVideo = this.props.videos[Math.floor(Math.random() * this.props.videos.length)];
+
+        return (
+            <React.Fragment>
+                <iframe title={selectedVideo.title} width={this.props.videoSize.width} height={this.props.videoSize.height} src={selectedVideo.src} frameBorder="0" allow={this.props.allow.join("; ")} allowFullScreen></iframe>
+            </React.Fragment>
+        )
+    }
+}
--- src/components/TextPekofyer.js
+++ src/components/TextPekofyer.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Pekofyer } from "text-rand-utils";
 
 import TextTransformer from "./TextTransformer";
+import EmbeddedVideoRandomizer from "./EmbeddedVideoRandomizer";
 
 export default class TextPekofyer extends React.Component {
     constructor(props) {
@@ -26,7 +27,7 @@ export default class TextPekofyer extends React.Component {
 
                 <div className="level">
                     <div className="level-item">
-                        <iframe title="peko-miko-song" width="560" height="315" src="https://www.youtube.com/embed/uRB1G0cKpIk" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                        <EmbeddedVideoRandomizer />
                     </div>
                 </div>
             </div>
